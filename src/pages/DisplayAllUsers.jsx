5f3b2d570cd22b7b18686a581b2819d2865e0492
Noel941 <noelarre22@gmail.com>
2025-01-27 20:28:30 +0000

    Update DisplayAllUsers.jsx
    redirect function

--- src/pages/DisplayAllUsers.jsx
+++ src/pages/DisplayAllUsers.jsx
@@ -1,42 +1,14 @@
 import { useOutletContext } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const DisplayAllUsers = () => {
   const { users: context } = useOutletContext();
   const { users, addUser } = context;
-  // const [state, setState] = useState(); how to display users?
+  const navigate = useNavigate();
 
-  // Function to deposit amount into user's balance
-  function handleButtonClickDeposit(userId, amount) {
-    if (amount <= 0) {
-      console.error("Deposit must be Positive");
-      return;
-    }
-    const userIndex = users.findIndex(user => user.id === userId);
-    if (userIndex !== -1) {
-      // context.users[userIndex].balance += amount;
-      // const newAmount = context.users[userIndex].balance + amount; how to update newAmount?
-      console.log(`Deposited ${amount} to user ${userId}. New Balance: ${context.users[userIndex].balance}`);
-    } else {
-      console.error("User Not Found");
-    }
-  };
-  // Function to withdraw amount from user's balance
-  function handleButtonClickWithdraw(userId, amount) {
-    if (amount <= 0) {
-      console.error("Withdrawal amount must be Positive");
-      return;
-    }
-    const userIndex = users.findIndex(user => user.id === userId);
-    if (userIndex !== -1) {
-      if (context.users[userIndex].balance >= amount) {
-        context.users[userIndex].balance -= amount;
-        console.log(`Withdrew ${amount} from user ${userId}. New balance: ${context.users[userIndex].balance}`);
-      } else {
-        console.error("Insufficient Funds");
-      }
-    } else {
-      console.error("User Not Found");
-    }
+  // Function to redirect users to their personal acount
+  const handleRedirect = (userId) => {
+    navigate(`/users/${userId}`);
   };
 
   return (
@@ -48,8 +20,7 @@ const DisplayAllUsers = () => {
             <p className="table items-center">{user.firstName}</p>
             <p className="table items-center">{user.lastName}</p>
             <p className="table items-center">{user.balance}</p>
-            <button className="table items-center" onClick={() => handleButtonClickDeposit(user.id, 1000)}>+</button>
-            <button className="table items-center" onClick={handleButtonClickWithdraw}>-</button>
+            <button className="table items-center" onClick={() => { handleRedirect(user.id) }}>Manage Account</button>
             {/* button is like a link tag when clinked would redirect to specific user profile */}
           </li>
         ))}
