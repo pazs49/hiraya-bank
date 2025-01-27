d5ab3d58b88f41cbecd3bb6e821e0aa504444883
Noel941 <noelarre22@gmail.com>
2025-01-27 18:50:12 +0000

    Update DisplayAllUsers.jsx

--- src/pages/DisplayAllUsers.jsx
+++ src/pages/DisplayAllUsers.jsx
@@ -3,16 +3,35 @@ import { useOutletContext } from "react-router-dom";
 const DisplayAllUsers = () => {
   const { users: context } = useOutletContext();
   const { users } = context;
+  const { addUser } = context;
+
+  function handleButtonClick(e) {
+
+  }
   return (
-    <div>
-      <ul>
+    <div className="overflow-x-auto">
+      <ul className="mx-auto">
         {users.map((user) => (
-          <li className="flex" key={user.id}>
+          <li className="flex justify-center my-4" key={user.id}>
             {/* Render the users here */}
+            <p className="table items-center">{user.firstName}</p>
+            <p className="table items-center">{user.lastName}</p>
+            <p className="table items-center">{user.balance}</p>
+            <button className="table items-center" onClick={handleButtonClick}>+</button>
+            {/* button is like a link tag when clinked would redirect to specific user profile */}
           </li>
         ))}
+        {/* {addUser.map((user) => (
+          <li className="flex justify-center my-4" key={user.id}>
+            <p className="table items-center">{user.firstName}</p>
+            <p className="table items-center">{user.lastName}</p>
+            <p className="table items-center">{user.balance}</p>
+            <button className="table items-center">san</button>
+          </li>
+        ))} */}
       </ul>
-    </div>
+    </div >
+
   );
 };
 export default DisplayAllUsers;
