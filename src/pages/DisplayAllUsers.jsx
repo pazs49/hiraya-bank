32e7eb593b960ae67e127c54d498519c83485a74
Noel941 <noelarre22@gmail.com>
2025-01-28 19:54:21 +0000

    Update DisplayAllUser

--- src/pages/DisplayAllUsers.jsx
+++ src/pages/DisplayAllUsers.jsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
 const DisplayAllUsers = () => {
   const { users: context } = useOutletContext();
   const { users, addUser } = context;
+  const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
+
+
 
   // Function to redirect users to their personal acount
   const handleRedirect = (userId) => {
     navigate(`/users/${userId}`);
   };
+  // Function to search
+  const filteredUsers = users.filter(user => {
+    if (!user || !user.name) return false; // Filter out users without a name
+    return user.name.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
 
   return (
     <div className="overflow-x-auto">
+      <input type="text" onChange={handleSearch} value={searchTerm} placeholder="Search User" />
+      {filteredUsers.map((user) => (
+        <p key={user.id}>{user.firstName}</p>
+      ))}
       <ul className="mx-auto">
         {users.map((user) => (
           <li className="flex justify-center my-4" key={user.id}>
@@ -25,6 +43,7 @@ const DisplayAllUsers = () => {
           </li>
         ))}
       </ul>
+
     </div >
 
   );
